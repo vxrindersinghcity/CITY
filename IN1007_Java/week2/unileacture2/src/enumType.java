6afb4c4081205bde875695266a519905f7f41585
vxrindersinghcity <Varinder.Singh@city.ac.uk>
2024-10-22 09:46:45 +0100

    enum

---
+++ IN1007_Java/week2/unileacture2/src/enumType.java
@@ -0,0 +1,24 @@
+public class enumType {
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+        THURSDAY, FRIDAY, SATURDAY
+    }
+
+    public static void main(String[] args) {
+        Day day = Day.SATURDAY;
+        switch (day) {
+            case MONDAY:
+                System.out.println("Mondays are bad.");
+                break;
+            case FRIDAY:
+                System.out.println("Fridays are better.");
+                break;
+            case SATURDAY: case SUNDAY:
+                System.out.println("Weekends are best.");
+                break;
+            default:
+                System.out.println("Midweek days are so-so.");
+                break;
+        }
+    }
+}
