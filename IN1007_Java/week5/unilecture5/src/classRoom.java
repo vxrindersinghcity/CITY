873f0c51a9e103d5e8671e3e31c08dca8d297cf4
vxrindersinghcity <Varinder.Singh@city.ac.uk>
2024-12-08 18:03:00 +0000

    updates

--- IN1007_Java/week5/unilecture5/src/classRoom.java
+++ IN1007_Java/week5/unilecture5/src/classRoom.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class classRoom {
     private int size;
     private Student rep;
+    private ArrayList<Student> stlist;
 
     public classRoom(int size, Student rep) {
         this.size = size;
