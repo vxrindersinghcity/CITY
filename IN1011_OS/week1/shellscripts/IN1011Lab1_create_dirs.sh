6e6f90128d0b97c4f18bb63e01831c178bd88c64
vxrindersinghcity <Varinder.Singh@city.ac.uk>
2025-02-17 12:47:54 +0000

    refactoring

---
+++ IN1011_OS/week1/shellscripts/IN1011Lab1_create_dirs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p IN1011_lab1_directories/{home,lib,usr/{local/lib,lib,bin},bin}
+cd IN1011_lab1_directories/bin
+touch my_foo.bin my_bar.bin
---
+++ IN1011_OS/week1/shellscripts/IN1011Lab1_destroy_dirs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -r IN1011_lab1_directories
