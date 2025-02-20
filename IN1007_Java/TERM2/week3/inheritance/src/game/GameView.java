28934d3c43609a337b79d8df55792e297e51cac7
vxrindersinghcity <Varinder.Singh@city.ac.uk>
2025-02-20 17:28:40 +0000

    update bg

--- IN1007_Java/TERM2/week3/inheritance/src/game/GameView.java
+++ IN1007_Java/TERM2/week3/inheritance/src/game/GameView.java
@@ -7,10 +7,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameView extends UserView {
-
-
+    private Image bg;
     public GameView(World w, int width, int height) {
         super(w, width, height);
+        bg=new ImageIcon("data/university.gif").getImage();
     }
-
+   @Override
+    public void paint(Graphics g) {
+        g.drawImage(bg,0,0,getWidth(),getHeight(),this);
+    }
 }
